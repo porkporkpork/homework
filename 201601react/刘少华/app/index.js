8dacdc1c87f1766d5985fb7036a5836923c59e70
shaohua.liu <fe.liushaohua@gmail.com>
2016-03-09 11:31:18 +0800

    提交第一天作业

---
+++ 201601react/刘少华/app/index.js
@@ -0,0 +1,14 @@
+/**
+ * @file 第一课作业
+ * @author liushaohua
+ * @email fe.liushaohua@gmail.com
+ */
+
+'use strict';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './container/App';
+import moduleB from './util/moduleA';
+
+let root = document.getElementById('app');
+ReactDOM.render( <App />, root );
